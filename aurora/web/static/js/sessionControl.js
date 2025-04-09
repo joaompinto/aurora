cb85b6c98684fc1280ad6aee228cb92255d37d80
João Pinto <lamego.pinto@gmail.com>
2025-04-09 17:41:31 +0200

    Remove initial Start New Chat and Load Previous buttons; always show input line

---
+++ aurora/web/static/js/sessionControl.js
@@ -0,0 +1,7 @@
+export function setupSessionControls() {
+  const footer = document.querySelector('.input-footer');
+  const inputLine = document.getElementById('input-line');
+
+  // Always show input line
+  inputLine.style.display = 'block';
+}
